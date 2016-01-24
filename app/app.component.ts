0e0fb6d7ea431c6c726c0290291c9e8adb6f22f5
Heling Zhao <heling.zhao@gmail.com>
2016-01-23 19:00:18 -0500

    Added boiler plate.

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
+
+@Component({
+     selector: 'app',
+     template: `
+        <h1>Kurjira Atsume</h1>
+     `
+        })
+export class AppComponent { }
