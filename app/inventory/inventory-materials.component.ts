9a75824ad6d4b5338834d55d3ad1db5af68f3d64
Heling Zhao <heling.zhao@gmail.com>
2016-01-27 10:50:45 -0500

    Updated inventory-service with a cabbage and inventory-materials with content. Please don't judge my commit messages.

--- app/inventory/inventory-materials.component.ts
+++ app/inventory/inventory-materials.component.ts
@@ -1,6 +1,47 @@
 import {Component, OnInit} from 'angular2/core';
+import {Router, RouteParams} from 'angular2/router';
+
+import {Resource, InventoryService} from './inventory.service';
 
 @Component({
-    template: '<h3>List of Materials</h3>'
+    template: `
+        <h3>List of Materials</h3>
+        <ul>
+            <li *ngFor="#material of materials"
+                    [class.selected]="isSelected(material)"
+                    (click)="onSelect(material)">
+                <img src="{{material.image}}">
+                <br>
+                <h4>Name: {{material.name}} </h4>
+                <h4>Quantity: {{material.quantity}} </h4>
+                <p *ngIf="isSelected(material)">{{material.description}}</p>
+            </li>
+        </ul>
+        
+    `,
 })
-export class MaterialsComponent { }
+export class MaterialsComponent implements OnInit {
+    materials: Resource[];
+    
+    private _selectedId: number;
+    
+    constructor(
+        private _service: InventoryService,
+        private _router: Router,
+        routeParams: RouteParams) {
+            this._selectedId = +routeParams.get('id');
+    }
+    
+    isSelected(material: Resource){
+        return material.id === this._selectedId;
+    }
+    
+    ngOnInit(){
+        this._service.getMaterials()
+            .then(materials => this.materials = materials);
+    }
+    
+    onSelect(material: Resource) {
+        this._router.navigate( ['Materials', {id: material.id}] );
+    }
+ }
--- app/inventory/inventory.service.ts
+++ app/inventory/inventory.service.ts
@@ -32,8 +32,9 @@ export class InventoryService {
 var resources = [
     new Resource(0, 'ingredient', 'Tomato', 4, 'It\'s a tomato. Deal with it.', 'http://images.all-free-download.com/images/graphicthumb/tomato_310436.jpg'),
     new Resource(1, 'ingredient', 'Potato', 2, 'Pot8to. PotAHto. Jagaimo.', 'http://images.all-free-download.com/images/graphicthumb/potatoes_clip_art_11374.jpg'),
-    new Resource(2, 'material', 'Glue', 1, 'A dab here, a dab there.', 'http://images.all-free-download.com/images/graphicthumb/glue_clip_art_23224.jpg'),
-    new Resource(3, 'material', 'Blue Paint', 1, 'It\'s not red paint. It\'s blue paint.', 'http://images.all-free-download.com/images/graphiclarge/bucket_of_paint_and_paintbrush_312403.jpg')
+    new Resource(2, 'ingredient', 'Cabbage', 5, 'Cabbage is the staple food in Poland. Delicious.' 'http://clipartfreefor.com/cliparts/cabbage-clip-art/cliparti1_cabbage-clip-art_08.jpg'),
+    new Resource(3, 'material', 'Glue', 1, 'A dab here, a dab there.', 'http://images.all-free-download.com/images/graphicthumb/glue_clip_art_23224.jpg'),
+    new Resource(4, 'material', 'Blue Paint', 1, 'It\'s not red paint. It\'s blue paint.', 'http://images.all-free-download.com/images/graphiclarge/bucket_of_paint_and_paintbrush_312403.jpg')
 ];
 
 var inventoryPromise = Promise.resolve(resources);
